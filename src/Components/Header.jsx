d40e7b85525f163d24915f81375d0b37e0c3e54e
Kyle Compton <kylecompton@hotmail.co.uk>
2025-02-10 17:29:49 +0000

    Basic Header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,22 @@
-
+import { Link } from "react-router-dom"
+import Logo from "../Assets/Logo.jpg"
 
 const Header = () => {
-
-}
+    return (
+      <header className="header">
+        <h1 className="title">Exhibitions</h1>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/artworks" className="nav-link">Artworks</Link>
+            </li>
+            <li>
+              <Link to="/exhibitions" className="nav-link">Exhibitions</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
 
 export default Header   
