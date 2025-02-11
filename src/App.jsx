bd55edf5f82616e5789182d76ff0032d7b601560
Kyle Compton <kylecompton@hotmail.co.uk>
2025-02-11 16:48:29 +0000

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Header from './Components/Header'
-import Home from './Components/Home'
-import Search from './Components/Search'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Search from "./Components/Search";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className='App'>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/search' element={<Search />}></Route>
-        </Routes>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+      <Footer />
     </div>
-  )
-  }
+  );
+}
 
-export default App
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 GalleryGate. All rights reserved.</p>
+      <nav>
+        <ul className="footer-links">
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/terms">Terms of Use</a>
+          </li>
+          <li>
+            <a href="/privacy">Privacy Policy</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
